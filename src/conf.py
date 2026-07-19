688303e7ecdb8a53a02ea2c2a8e6a1f34bd18541
Zane Selvans <zane.selvans@catalyst.coop>
2026-07-19 06:50:59 -0600

    Add social links to top level docs index

--- src/conf.py
+++ src/conf.py
@@ -29,3 +29,37 @@ html_theme = "pydata_sphinx_theme"
 html_logo = "_static/catalyst_logo-200x200.png"
 html_icon = "_static/favicon.ico"
 html_static_path = ["_static"]
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/catalyst-cooperative",
+            "icon": "fa-brands fa-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Mastodon",
+            "url": "https://mastodon.energy/@catalystcoop",
+            "icon": "fa-brands fa-mastodon",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Bluesky",
+            "url": "https://bsky.app/profile/catalyst.coop",
+            "icon": "fa-brands fa-bluesky",
+            "type": "fontawesome",
+        },
+        {
+            "name": "LinkedIn",
+            "url": "https://www.linkedin.com/company/catalyst-cooperative/",
+            "icon": "fa-brands fa-linkedin",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Support Catalyst Cooperative",
+            "url": "https://opencollective.com/pudl",
+            "icon": "fa-solid fa-donate",
+            "type": "fontawesome",
+        },
+    ],
+}
